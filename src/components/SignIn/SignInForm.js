5cf8d8e8a2cc64b70aa1fcf0b7e70502bad44fe5
Martin Simov <martin.simov@microinvest.net>
2021-11-16 13:20:48 +0200

    Added Styling to Sign In Form

--- src/components/SignIn/SignInForm.js
+++ src/components/SignIn/SignInForm.js
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import { FirebaseContext } from "../../context";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
@@ -19,8 +19,6 @@ const SignInForm = () => {
   const { register, handleSubmit, reset, formState } = useForm(formOptions);
   const { errors } = formState;
 
-  const [error, setError] = useState(null);
-
   const onSubmit = (data) => {
     const { email, password } = data;
 
@@ -31,31 +29,80 @@ const SignInForm = () => {
         console.log("Success");
       })
       .catch((error) => {
-        setError(error);
+        reset();
+        console.log("Error");
       });
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input
-        name="email"
-        type="text"
-        placeholder="Email Address"
-        {...register("email")}
-      />
-      <p>{errors.email?.message}</p>
-
-      <input
-        name="password"
-        type="password"
-        placeholder="Password"
-        {...register("password")}
-      />
-      <p>{errors.password?.message}</p>
-
-      <button type="submit">Sign In</button>
-
-      {error && <p>{error.message}</p>}
+    <form
+      className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <div className="mb-4">
+        <label
+          className="block text-gray-700 text-sm font-bold mb-2"
+          htmlFor="email"
+        >
+          Email Address
+        </label>
+        <input
+          className={`shadow appearance-none border ${
+            errors.email && "border-red-500"
+          } rounded w-full py-2 px-3 text-gray-700 ${
+            errors.email && "mb-3"
+          } leading-tight focus:outline-none focus:shadow-outline`}
+          id="email"
+          name="email"
+          type="email"
+          autoComplete="email"
+          placeholder="Email Address"
+          {...register("email")}
+        />
+        {errors.email && (
+          <p className="text-red-500 text-xs italic">{errors.email.message}</p>
+        )}
+      </div>
+      <div className="mb-6">
+        <label
+          className="block text-gray-700 text-sm font-bold mb-2"
+          htmlFor="password"
+        >
+          Password
+        </label>
+        <input
+          className={`shadow appearance-none border ${
+            errors.password && "border-red-500"
+          } rounded w-full py-2 px-3 text-gray-700 ${
+            errors.password && "mb-3"
+          } leading-tight focus:outline-none focus:shadow-outline`}
+          id="password"
+          name="password"
+          type="password"
+          autoComplete="current-password"
+          placeholder="Password"
+          {...register("password")}
+        />
+        {errors.password && (
+          <p className="text-red-500 text-xs italic">
+            {errors.password.message}
+          </p>
+        )}
+      </div>
+      <div className="flex items-center justify-between">
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          type="submit"
+        >
+          Sign In
+        </button>
+        <a
+          className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+          href="#"
+        >
+          Forgot Password?
+        </a>
+      </div>
     </form>
   );
 };
